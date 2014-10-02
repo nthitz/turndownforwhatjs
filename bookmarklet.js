ff876d0a67f7d90a4c39ff29858a8fc4014d1467
Nick Yahnke <nthitz@gmail.com>
2014-10-01 21:40:31 -0700

    fixed bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js/tdfw.js');document.body.appendChild(s);})();
+javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js');document.body.appendChild(s);})();
