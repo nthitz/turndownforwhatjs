06d093387dd245dc05efd2212ad5ef91889e3dd6
Drew Tuckey <77762391+aptuckey@users.noreply.github.com>
2023-01-28 18:18:54 -0500

    Update bookmarklet.js
    Modify Extension

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js');document.body.appendChild(s);})();
+javascript:(function(){javascript:var s=document.createElement('script');s.setAttribute('src','https://aptuckey.github.io/fireflies/ff.js');document.body.appendChild(s);})();
